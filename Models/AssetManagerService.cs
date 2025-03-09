b8e5ab287ad1ee418a35ad9a3f91de4068411b1d
eduardgangan <114904168+eduard-gangan@users.noreply.github.com>
2025-03-09 15:53:49 +0100

    Fixed file structure

---
+++ Models/AssetManagerService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using IronXL;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using HeatOptimization.Models;
+
+namespace HeatOptimisation.Services
+{
+    public class AssetManagerService
+    {
+        private readonly List<ProductionUnit> _productionUnits;
+
+        public AssetManagerService()
+        {
+            _productionUnits = new List<ProductionUnit>
+            {
+                new ProductionUnit("Gas Boiler 1", 4.0, null, 520.0, 175.0, 0.9, "Gas"),
+                new ProductionUnit("Gas Boiler 2", 3.0, null, 560.0, 130.0, 0.7, "Gas"),
+                new ProductionUnit("Oil Boiler 1", 4.0, null, 670.0, 330.0, 1.5, "Oil"),
+                new ProductionUnit("Gas Motor 1", 3.5, 2.6, 990.0, 650.0, 1.8, "Gas"),
+                new ProductionUnit("HP1", 6.0, -6.0, 60.0, 0, 0, "Electricity")
+            };
+        }
+
+        public List<ProductionUnit> GetProdUnits()
+        {
+            return _productionUnits;
+        }
+    }
+}
---
+++ Models/DataConverter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using IronXL;
+
+namespace HeatOptimisation.Models
+{
+    public class DataConverter
+    {
+        static readonly WorkBook workbook = WorkBook.LoadCSV("../Assets/2025 Heat Production Optimization - Danfoss Deliveries - Source Data Manager.csv");
+        readonly WorkSheet workSheet = workbook.GetWorkSheet("Heat Demands");
+
+
+        private void LoadData()
+        {
+            foreach (var cell in workSheet)
+            {
+
+            }
+        }
+    }
+}
