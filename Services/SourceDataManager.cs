0a5c36a4c5ec0ec407588dc87578a74d0dd5e7f1
Daimnd1 <dchit24@student.sdu.dk>
2025-04-15 19:53:55 +0200

    Created unit tests for SDM and fixed the SDM

--- Services/SourceDataManager.cs
+++ Services/SourceDataManager.cs
@@ -17,38 +17,38 @@ namespace SP2.Services
         public static List<TimeSeriesData> LoadData(string csvPath)
         {
             var records = new List<TimeSeriesData>();
-
+            
             using var reader = new StreamReader(csvPath);
             using var csv = new CsvReader(reader, new CsvConfiguration(CultureInfo.InvariantCulture)
             {
                 HasHeaderRecord = false,
                 MissingFieldFound = null,
                 IgnoreBlankLines = true
-
             });
 
             csv.Read(); csv.Read();
 
             while (csv.Read())
             {
-                if(DateTime.TryParse(csv.GetField(0), out var winterTimeFrom) &&
-                    DateTime.TryParse(csv.GetField(1), out var winterTimeTo) &&
+                // Read winter period data
+                if (DateTime.TryParseExact(csv.GetField(0), "M/d/yyyy H:mm", CultureInfo.InvariantCulture, DateTimeStyles.None, out var winterTimeFrom) &&
+                    DateTime.TryParseExact(csv.GetField(1), "M/d/yyyy H:mm", CultureInfo.InvariantCulture, DateTimeStyles.None, out var winterTimeTo) &&
                     double.TryParse(csv.GetField(2), out var winterHeatDemand) &&
                     double.TryParse(csv.GetField(3), out var winterElectricityPrice))
                 {
                     records.Add(new TimeSeriesData(winterTimeFrom, winterTimeTo, winterHeatDemand, winterElectricityPrice));
                 }
 
-                if (DateTime.TryParse(csv.GetField(5), out var summerTimeFrom) &&
-                    DateTime.TryParse(csv.GetField(6), out var summerTimeTo) &&
+                // Read summer period data
+                if (DateTime.TryParseExact(csv.GetField(5), "M/d/yyyy H:mm", CultureInfo.InvariantCulture, DateTimeStyles.None, out var summerTimeFrom) &&
+                    DateTime.TryParseExact(csv.GetField(6), "M/d/yyyy H:mm", CultureInfo.InvariantCulture, DateTimeStyles.None, out var summerTimeTo) &&
                     double.TryParse(csv.GetField(7), out var summerHeatDemand) &&
                     double.TryParse(csv.GetField(8), out var summerElectricityPrice))
                 {
                     records.Add(new TimeSeriesData(summerTimeFrom, summerTimeTo, summerHeatDemand, summerElectricityPrice));
                 }
-
             }
-            
+
             return records;
         }
     }
